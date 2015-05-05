c46020f96142ca08b1eed466df0baea018cf83ad
Jerome Obba <auchomage@hotmail.co.uk>
2015-05-05 02:50:51 +0100

    updated files

---
+++ mixedstreets2/site/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+
+// Pass the Express instance to the routes module
+var routes = require('./routes')(app);
+
+app.set('port', process.env.PORT || 80);
+
+// Set the view engine
+app.set('view engine', 'jade');
+// Where to find the view files
+app.set('views', './views');
+
+// Use the 'static' middleware
+app.use(express.static(__dirname + '/public'));
+
+// HTML should be prettified
+app.locals.pretty = true;
+
+// custom 404 page
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404).send('404 - File Not Found');
+});
+
+// custom 500 page
+app.use(function(err, req,res, next){
+//	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500).send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http:// localhost: ' + 
+		app.get('port') + '; press-Control C to terminate' );
+});
